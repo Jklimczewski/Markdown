e0bf613f1e10abfd4cb28998672c77f8cb5a0044
Jklimczewski <92151596+Jklimczewski@users.noreply.github.com>
2022-01-24 19:47:32 +0100

    Create test_main2.py

---
+++ test_main2.py
@@ -0,0 +1,12 @@
+import main2
+
+def test_roznica():
+    assert roznica(2, 1) == 1
+    assert roznica(2, 2) == 0
+    assert roznica(2.5, 2) == 0.5
+
+
+def test_Wypisz2():
+    assert Wypisz2("jabłkox") == "Jabłkox"
+    assert Wypisz2("Bananx") == banan
+    assert Wypisz2("piesx") == piesx
